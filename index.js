3bf575f57fbe7c5cf80f3ee8e04ad180d7c54634
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2021-11-07 23:37:56 +0200

    [05] Module 5

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000, () => console.log('Server started on the port: 3000'));
