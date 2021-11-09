6c64f773ebb2cd51126b88eb346993e4fc08429b
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2021-11-09 23:22:09 +0200

    [07] Module 7

---
+++ controllers/error.js
@@ -0,0 +1,7 @@
+const getPageNotFound = (req, res, next) => {
+  res
+    .status(404)
+    .render('404', { pageTitle: 'Page Not Founds', path: '/not-found' });
+};
+
+module.exports = { getPageNotFound };
