8af40a58d8d81437fe6417be1491fc75a72f7679
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2021-11-09 21:54:23 +0200

    [06] Module 6

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const path = require('path');
 
 const express = require('express');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res
+    .status(404)
+    .render('404', { pageTitle: 'Page Not Founds', path: '/not-found' });
 });
 
-app.listen(3000, () => console.log('Server started on the port: 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
